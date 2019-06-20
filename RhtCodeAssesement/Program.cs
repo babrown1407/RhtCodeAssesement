7b8e4947248733b9573477d19fff91c0d0a78eb6
Bradley Brown <bbrown242@email.itt-tech.edu>
2019-06-19 22:39:59 -0400

    Initial project set up

---
+++ RhtCodeAssesement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RhtCodeAssesement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
