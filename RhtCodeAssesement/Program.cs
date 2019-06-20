392996357508a0faad73c971f8585633de88a867
Bradley Brown <bbrown242@email.itt-tech.edu>
2019-06-20 01:03:21 -0400

    Started initial work on project, seperated methods, try to clean up code

--- RhtCodeAssesement/Program.cs
+++ RhtCodeAssesement/Program.cs
@@ -2,11 +2,30 @@
 
 namespace RhtCodeAssesement
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string myString;
+
+            Console.Write("Please Enter a String : ");
+            myString = Console.ReadLine();
+
+            var hello = ReverseString(myString);          
+            
+
+            Console.WriteLine(hello);
+            Console.ReadLine();
+        }
+
+        public static string ReverseString(string rString)
+        {
+            string[] words = rString.Split(' ');
+            Array.Reverse(words);
+
+            var jointWord = string.Join(" ", words);
+
+            return jointWord;
         }
     }
 }
